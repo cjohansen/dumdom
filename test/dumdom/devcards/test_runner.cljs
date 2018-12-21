dbae073fe13e047c917d87d1e0a89b04a7407e2d
Christian Johansen <christian@cjohansen.no>
2018-12-21 12:45:34 +0100

    Require correct ns for the devcards test runner

--- test/dumdom/devcards/test_runner.cljs
+++ test/dumdom/devcards/test_runner.cljs
@@ -5,24 +5,3 @@
             [dumdom.dom-test]))
 
 (enable-console-print!)
-
-;; (defn color-favicon-data-url [color]
-;;   (let [cvs (.createElement js/document "canvas")]
-;;     (set! (.-width cvs) 16)
-;;     (set! (.-height cvs) 16)
-;;     (let [ctx (.getContext cvs "2d")]
-;;       (set! (.-fillStyle ctx) color)
-;;       (.fillRect ctx 0 0 16 16))
-;;     (.toDataURL cvs)))
-
-;; (defn change-favicon-to-color [color]
-;;   (let [icon (.getElementById js/document "favicon")]
-;;     (set! (.-href icon) (color-favicon-data-url color))))
-
-;; (defmethod report [:_devcards_test_card_reporter :summary] [m]
-;;   (println "\nRan" (:test m) "tests containing"
-;;            (+ (:pass m) (:fail m) (:error m)) "assertions.")
-;;   (println (:fail m) "failures," (:error m) "errors.")
-;;   (if (< 0 (+ (:fail m) (:error m)))
-;;     (change-favicon-to-color "#d00")
-;;     (change-favicon-to-color "#0d0")))
