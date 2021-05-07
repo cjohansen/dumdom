e32a1628215cebdb1afc81abf1afa81897a15400
Magnar Sveen <magnars@gmail.com>
2021-05-07 12:53:40 +0200

    Include snabbdom externs
    Fixes #11

---
+++ src/snabbdom/externs.js
@@ -0,0 +1,49 @@
+var vdomNode = {
+  /** @type {Array<vdomNode>} */
+  children: [],
+  data: {
+    attrs: {},
+    hook: {
+      destroy: function () {},
+      update: function () {},
+      insert: function () {}
+    }
+  },
+  elm: {},
+  key: {},
+  listener: function () {},
+  sel: "",
+  text: ""
+};
+
+var snabbdom = {
+  /**
+   * @return {vdomNode}
+   */
+  h: function () {},
+  init: function () {},
+  thunk: function () {},
+  props: {
+    create: function () {},
+    update: function () {}
+  },
+  attributes: {
+    create: function () {},
+    update: function () {}
+  },
+  eventlisteners: {
+    create: function () {},
+    update: function () {},
+    destroy: function () {}
+  },
+  style: {
+    create: function () {},
+    update: function () {},
+    destroy: function () {},
+    remove: function () {}
+  },
+  /**
+   * @return {vdomNode}
+   */
+  tovnode: function () {}
+};
