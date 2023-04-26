92d6cda834ac8fd35aada9472de804379c7c3a48
Christian Johansen <christian@cjohansen.no>
2023-04-26 10:41:19 +0200

    Add snabbdom as a NPM dep

--- src/deps.cljs
+++ src/deps.cljs
@@ -2,4 +2,5 @@
  [{:file "snabbdom/snabbdom.js"
    :file-min "snabbdom/snabbdom.min.js"
    :provides ["snabbdom"]}]
- :externs ["snabbdom/externs.js"]}
+ :externs ["snabbdom/externs.js"]
+ :npm-deps {"snabbdom" "3.5.1"}}
